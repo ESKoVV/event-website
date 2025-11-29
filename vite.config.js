e42a4a964e1b1670c5c611d4c55de75f9a3aed60
ESKoVV <koksiccotan@gmail.com>
2025-11-29 14:44:48 +0300

    1.0.2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/event-website/', // Замените 'event-website' на имя вашего репозитория
+  base: '/event-website/', // Важно: имя вашего репозитория
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: 'dist'
   }
 })
