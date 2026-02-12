b63461353af49ac9739d8762e67daee0d7accc49
ESKoVV <koksiccotan@gmail.com>
2026-02-12 19:55:58 +0300

    1.1.1

--- src/router/views/EventView.vue
+++ src/router/views/EventView.vue
@@ -74,7 +74,7 @@
 import { computed, onMounted, ref, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useSupabase } from '../../composables/useSupabase.js'
-import EventPhotoModal from '../components/EventPhotoModal.vue'
+import EventPhotoModal from '../../components/EventPhotoModal.vue'
 
 const route = useRoute()
 const router = useRouter()
